cd5e98882084ca009ed4870464c5303102877d80
Olajide Eniseyin <eniseyinolajide@gmail.com>
2025-10-03 11:26:08 +0100

    Added mail and email view to handle otp emails

---
+++ resources/views/mail/otp.blade.php
@@ -0,0 +1,14 @@
+{{-- Plain, clear message. 
+     If you prefer, you can wrap in HTML/CSS for a prettier template. --}}
+<p>Hello,</p>
+
+<p>Your one-time verification code is:</p>
+
+<h2 style="font-size: 28px; letter-spacing: 4px;">{{ $otp }}</h2>
+
+<p>This code will expire in 5 minutes.</p>
+
+<p>If you did not request this, please ignore this email.</p>
+
+<p>Thanks,<br>
+The Team</p>
