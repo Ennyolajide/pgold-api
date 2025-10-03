449471f4d55c0e8cffc3888ab07664fa96c9d412
Olajide Eniseyin <eniseyinolajide@gmail.com>
2025-10-03 11:24:37 +0100

    Added a base controller to handle responses

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,45 @@
+<?php
+  
+namespace App\Http\Controllers\API;
+  
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller as Controller;
+  
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'status' => 'success',
+            'data'    => $result,
+            'message' => $message,
+        ];
+  
+        return response()->json($response, 200);
+    }
+  
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'status'=>'error',
+            'message' => $error,
+        ];
+  
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+  
+        return response()->json($response, $code);
+    }
+}
