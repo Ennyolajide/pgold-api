30d02f5a4a1830ee35295c740b3dc5f7a7215ae4
Olajide Eniseyin <eniseyinolajide@gmail.com>
2025-10-03 11:12:20 +0100

    Added User model, migration & factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'username' => $this->faker->unique()->userName(),
+            'full_name' => $this->faker->name(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => null,
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
+            'referral_code' => null,
+            'heard_about' => null,
+            'selfie' => null,
         ];
     }
 
